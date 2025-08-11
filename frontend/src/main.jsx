423ee423af12a98c5895b2ac4d671583f569e0e9
Dev Raj Dev <thakurdev2672@gmail.com>
2025-08-11 23:53:17 +0530

    query client added in main.jsx just wrapup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from "@/components/ui/sonner"
+import {
+  QueryClient,
+  QueryClientProvider,
+ 
+} from '@tanstack/react-query'
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
+    <QueryClientProvider client={queryClient}>
     <App />
-    <Toaster />
-  </StrictMode>,
+    </QueryClientProvider>,
+                       
 )
